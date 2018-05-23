caaac508b587953a0057eeb089cb6cfffdd9cf45
wzxjiang <wzxjiang@foxmail.com>
2018-05-23 16:48:55 +0800

    update diff foundation & add unit test

--- Sources/Diff/Diff.swift
+++ Sources/Diff/Diff.swift
@@ -1,17 +1,54 @@
+//
+//  Diff.swift
+//  Diff
+//
+//  Created by WzxJiang on 18/5/23.
+//  Copyright © 2018年 WzxJiang. All rights reserved.
+//
+//  https://github.com/Wzxhaha/Diff
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+
 extension String {
-    subscript (i: Int) -> Character {
-        return self[index(startIndex, offsetBy: i)]
+    /// Return character in string
+    ///
+    /// - Parameter idx: index
+    subscript (idx: Int) -> Character {
+        return self[index(startIndex, offsetBy: idx)]
     }
 }
 
 typealias Matrix = [[Int]]
 
-func draw(from: String, to: String) -> [[Int]] {
-    let l = from.utf8.count + 1
-    let m = to.utf8.count + 1
-    var result: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: l)
-    for i in 1..<l {
-        for j in 1..<m {
+/// Draw LCS matrix with two strings
+///
+/// - Parameters:
+///   - from: string
+///   - to: string that be compared
+/// - Returns: matrix
+func drawMatrix(from: String, to: String) -> Matrix {
+    let row = from.utf8.count + 1
+    let column = to.utf8.count + 1
+    var result: [[Int]] = Array(repeating: Array(repeating: 0, count: column), count: row)
+    for i in 1..<row {
+        for j in 1..<column {
             if from[i-1] == to[j-1] {
                 result[i][j] = result[i-1][j-1] + 1
             } else {
@@ -22,52 +59,104 @@ func draw(from: String, to: String) -> [[Int]] {
     return result
 }
 
-func lcs(from: String, to: String, i: Int, j: Int, matrix: Matrix, change: (from: [Int], to: [Int])) -> (from: [Int], to: [Int]) {
-    if i == 0 || j == 0 {
-        return change
+typealias Position = (row: Int, column: Int)
+
+/// LCS
+///
+/// - Parameters:
+///   - from: string
+///   - to: string that be compared
+///   - position: current position
+///   - matrix: matrix
+///   - same: same character's indexes
+/// - Returns: same character's indexes
+func lcs(from: String, to: String, position: Position, matrix: Matrix, same: (from: [Int], to: [Int])) -> (from: [Int], to: [Int]) {
+    if position.row == 0 || position.column == 0 {
+        return (same.from.reversed(), same.to.reversed())
     }
-    if from[i-1] == to[j-1] {
-        return lcs(from: from, to: to, i: i - 1, j: j - 1, matrix: matrix, change: (change.from + [i-1], change.to + [j-1]))
-    } else if matrix[i-1][j] >= matrix[i][j-1] {
-        return lcs(from: from, to: to, i: i - 1, j: j, matrix: matrix, change: change)
+    if from[position.row-1] == to[position.column-1] {
+        return lcs(from: from, to: to, position: (position.row - 1, position.column - 1), matrix: matrix, same: (same.from + [position.row - 1], same.to + [position.column - 1]))
+    } else if matrix[position.row-1][position.column] >= matrix[position.row][position.column-1] {
+        return lcs(from: from, to: to, position: (position.row - 1, position.column), matrix: matrix, same: same)
     } else {
-        return lcs(from: from, to: to, i: i, j: j - 1, matrix: matrix, change: change)
+        return lcs(from: from, to: to, position: (position.row, position.column - 1), matrix: matrix, same: same)
+    }
+}
+
+extension Array where Element == Int {
+    /// Get [Range<Int>] with [Int]
+    /// - example: [0, 3, 4] -> [0...0, 3...4]
+    func getSameRanges() -> [CountableClosedRange<Int>] {
+        var ranges: [CountableClosedRange<Int>] = []
+        var begin = first ?? 0
+        enumerated().forEach { (idx, current) in
+            guard count > idx + 1 else {
+                ranges.append(begin...current)
+                return
+            }
+            
+            let next = self[idx + 1]
+            if next - current != 1 {
+                ranges.append(begin...current)
+                begin = next
+            }
+        }
+        return ranges
+    }
+    
+    /// Get [Range<Int>] with [Int]
+    /// - example: [0, 3, 4] -> [1...2]
+    func getChangeRanges(max: Int) -> [CountableClosedRange<Int>] {
+        var ranges: [CountableClosedRange<Int>] = []
+        var begin = first ?? 0
+        if begin != 0 {
+            ranges.append(0...(begin - 1))
+        }
+        enumerated().forEach { (idx, current) in
+            guard count > idx + 1 else {
+                if current != max {
+                    ranges.append((current + 1)...max)
+                }
+                return
+            }
+
+            let next = self[idx + 1]
+            if next - current != 1 {
+                ranges.append((current + 1)...(next - 1))
+            }
+            begin = next
+        }
+        if count == 0, max >= 0 {
+            ranges.append(0...max)
+        }
+        return ranges
+    }
+}
+
+public struct Modification {
+    let add: [CountableClosedRange<Int>]
+    let delete: [CountableClosedRange<Int>]
+    let same: [CountableClosedRange<Int>]
+    
+    init(from: String, to: String, matrix: Matrix) {
+        let same =
+            lcs(from: from, to: to, position: (from.utf8.count, to.utf8.count), matrix: matrix, same: ([], []))
+        add = same.from.getChangeRanges(max: from.utf8.count - 1)
+        delete = same.to.getChangeRanges(max: to.utf8.count - 1)
+        self.same = same.from.getSameRanges()
     }
 }
 
 public struct Diff {
     public let from: String
     public let to: String
-    public let change: Change
+    public let modification: Modification
     let matrix: Matrix
     
     public init(from: String, to: String) {
         self.from = from
         self.to = to
-        self.matrix = draw(from: from, to: to)
-        self.change = Change(from: from, to: to, matrix: matrix)
-    }
-}
-
-public struct Change {
-    let from: [Range<Int>]
-    let to: [Range<Int>]
-    
-    init(from: String, to: String, matrix: Matrix) {
-        let change = lcs(from: from, to: to, i: from.utf8.count, j: to.utf8.count, matrix: matrix, change: ([], []))
-        
-        let fromRange: [Range<Int>] = []
-        let toRange: [Range<Int>] = []
-        
-        change.from.reversed().forEach {
-            print($0)
-        }
-        
-        change.to.reversed().forEach {
-            print($0)
-        }
-        
-        self.from = fromRange
-        self.to = toRange
+        matrix = drawMatrix(from: from, to: to)
+        modification = Modification(from: from, to: to, matrix: matrix)
     }
 }
--- Tests/DiffTests/DiffTests.swift
+++ Tests/DiffTests/DiffTests.swift
@@ -2,22 +2,150 @@ import XCTest
 @testable import Diff
 
 class DiffTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let x = "abcd"
-        let y = "acd"
-        let diff = Diff(from: x, to: y)
-        diff.matrix.forEach {
-            print($0)
-        }
+    func testMatrix() {
+        assert(
+            Diff(from: "abcd", to: "acd").matrix == [
+                [0, 0, 0, 0],
+                [0, 1, 1, 1],
+                [0, 1, 1, 1],
+                [0, 1, 2, 2],
+                [0, 1, 2, 3]
+            ]
+        )
         
+        assert(
+            Diff(from: "abcdegh", to: "ae").matrix == [
+                [0, 0, 0],
+                [0, 1, 1],
+                [0, 1, 1],
+                [0, 1, 1],
+                [0, 1, 1],
+                [0, 1, 2],
+                [0, 1, 2],
+                [0, 1, 2]
+            ]
+        )
         
+        assert(
+            Diff(from: "adf", to: "d").matrix == [
+                [0, 0],
+                [0, 0],
+                [0, 1],
+                [0, 1],
+            ]
+        )
+        
+        assert(
+            Diff(from: "d", to: "adf").matrix == [
+                [0, 0, 0, 0],
+                [0, 0, 1, 1]
+            ]
+        )
+        
+        assert(
+            Diff(from: "", to: "").matrix == [
+                [0],
+            ]
+        )
     }
 
+    func testRange() {
+        assert(
+            [0, 1, 3].getSameRanges() == [
+                0...1,
+                3...3
+            ]
+        )
+        
+        assert(
+            [0, 1, 2, 3].getSameRanges() == [
+                0...3
+            ]
+        )
+        
+        assert(
+            [3, 5].getSameRanges() == [
+                3...3,
+                5...5
+            ]
+        )
+        
+        assert(
+            [5, 6, 7].getSameRanges() == [
+                5...7
+            ]
+        )
+        
+        assert(
+            [].getSameRanges() == []
+        )
+        
+        assert(
+            [0, 1, 3].getChangeRanges(max: 10) == [
+                2...2,
+                4...10
+            ]
+        )
+        
+        assert(
+            [0, 1, 2, 3].getChangeRanges(max: 10) == [
+                4...10
+            ]
+        )
+        
+        assert(
+            [3, 5].getChangeRanges(max: 10) == [
+                0...2,
+                4...4,
+                6...10
+            ]
+        )
+        
+        assert(
+            [5, 6, 7].getChangeRanges(max: 10) == [
+                0...4,
+                8...10,
+            ]
+        )
+        
+        assert(
+            [].getChangeRanges(max: 10) == [
+                0...10
+            ]
+        )
+    }
+    
+    func testString1() {
+        let from = "abcdhijk"
+        let to = "bj"
+        let diff = Diff(from: from, to: to)
+        assert(
+            (from as NSString).substring(with: NSRange(diff.modification.add[0])) == "a" &&
+            (from as NSString).substring(with: NSRange(diff.modification.add[1])) == "cdhi" &&
+            (from as NSString).substring(with: NSRange(diff.modification.add[2])) == "k" &&
+            diff.modification.delete.count == 0 &&
+            (from as NSString).substring(with: NSRange(diff.modification.same[0])) == "b" &&
+            (from as NSString).substring(with: NSRange(diff.modification.same[1])) == "j")
+    }
+    
+    func testString2() {
+        let from = "abcdhijk"
+        let to = "bexj"
+        let diff = Diff(from: from, to: to)
+        assert(
+            (from as NSString).substring(with: NSRange(diff.modification.add[0])) == "a" &&
+            (from as NSString).substring(with: NSRange(diff.modification.add[1])) == "cdhi" &&
+            (from as NSString).substring(with: NSRange(diff.modification.add[2])) == "k" &&
+            (to as NSString).substring(with: NSRange(diff.modification.delete[0])) == "ex" &&
+            (from as NSString).substring(with: NSRange(diff.modification.same[0])) == "b" &&
+            (from as NSString).substring(with: NSRange(diff.modification.same[1])) == "j"
+        )
+    }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testMatrix", testMatrix),
+        ("testRange", testRange),
+        ("testString1", testString1),
+        ("testString2", testString2)
     ]
 }
